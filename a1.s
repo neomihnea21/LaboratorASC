83f916f94efa98dddacce308605376b2724c335c
Neacsu Mihnea-Valentin <145196101+neomihnea21@users.noreply.github.com>
2023-12-23 13:32:24 +0200

    E COMPLET, OMULE!!

--- a1.s
+++ a1.s
@@ -1,10 +1,12 @@
 .data
-  matrix: .space 2200 # A MERS, intrebarea e unde scrie cand dai printf
+  matrix: .space 2200 
   newMatrix: .space 2200
   line: .space 4
   column: .space 4
   m: .space 4
   n: .space 4
+  deepN: .space 4
+  deepM: .space 4
   k: .space 4
   i: .space 4
   pos: .space 4
@@ -12,6 +14,7 @@
   produs: .space 8
   formatRead: .asciz "%ld"
   formatWrite: .asciz "%ld "
+  formatString: .asciz "%s"
   newline: .asciz "\n"
   lineIndex: .long 0
   columnIndex: .long 0
@@ -19,17 +22,24 @@
 
 .global main
 main:
+   
    pushl $m
    pushl $formatRead
    call scanf
    popl %ebx
    popl %ebx
    
+   movl m, %ebx
+   movl %ebx, deepM #trebuie sa jucam din doua, pentru ca mov a, b, e eroare
+   
    pushl $n
    pushl $formatRead
    call scanf
    popl %ebx
    popl %ebx
+   
+   movl n, %ebx
+   movl %ebx, deepN
    
    pushl $p
    pushl $formatRead
@@ -38,10 +48,13 @@ main:
    popl %ebx
    
    movl $0, i
-lea matrix, %edi  
+lea matrix, %edi
 initializare:
   movl m, %eax
-  mull n
+  addl $2, %eax
+  movl n, %ebx
+  addl $2, %ebx
+  mull %ebx
   # stocam lungimea matricei intr-o variabila, ca nu poti sa compari expresii matematice cu cmp
   movl %eax, produs
   movl i, %ebx
@@ -78,9 +91,11 @@ citire:
    
    movl line, %eax
    movl $0, %edx
+   addl $2, n
    mull n
+   subl $2, n
    addl column, %eax
-   
+   incl %eax
    lea matrix, %edi
    movl $1, (%edi, %eax, 4)
    
@@ -103,35 +118,41 @@ continuare:
     # daca am facut k generatii, scrie ce avem
     #parcurgem matricea cu variabilele lineIndex si columnIndex
     movl $1, lineIndex
+    
+    movl deepM, %ebx
+    movl %ebx, m
+    movl deepN, %ebx
+    movl %ebx, n
    C_linii:
      movl lineIndex, %ebx
-     decl %ebx
      cmp %ebx, m
-     je finalPas
-     incl %ebx
+     jl finalPas
      movl $1, columnIndex
      #parcurgem linia la care suntem in lineIndex
      C_linieCurenta:
        movl columnIndex, %ebx
-       decl %ebx
        cmp n, %ebx
-       je finalGeneratie
-       incl %ebx
+       jg finalGeneratie
        movl lineIndex, %eax
        xorl %edx, %edx
+       addl $2, n
        mull n
+       movl deepN, %ebx
+       movl %ebx, n  # refacerea in doua miscari a lui n
        addl columnIndex, %eax
+       incl %eax
        #am scos in eax indicele de lucru, in ecx va fi valoarea celulei de la indice
        movl $newMatrix, %ebx
        lea matrix, %edi
        movl %eax, %ecx
+       xorl %edx, %edx
        generatieUrmatoare:
          #edx e gata 0
          verificaVecini:
            #vom intoarce prin edx numarul de vecini vii
            #incepem cu un soi de vecin 0, stanga-sus
            subl n, %ecx
-           subl $1, %ecx
+           subl $3, %ecx
            cmp $1, (%edi, %ecx, 4)
            jne vecin1
            incl %edx
@@ -151,12 +172,14 @@ continuare:
            vecin3:
               #dreapta-centru
               addl n, %ecx
+              addl $2, %ecx
               cmp $1, (%edi, %ecx, 4)
               jne vecin4
               incl %edx
            vecin4:
               #dreapta-jos
               addl n, %ecx
+              addl $2, %ecx
               cmp $1, (%edi, %ecx, 4)
               jne vecin5
               incl %edx
@@ -175,6 +198,7 @@ continuare:
             vecin7:
               #stanga-centru
               subl n, %ecx
+              subl $2, %ecx
               cmp $1, (%edi, %ecx, 4)
               jne terminaVecini
               incl %edx
@@ -194,16 +218,16 @@ continuare:
              je ramane_vie
          omoara_celula:
              # vom pune 0 in noua matrice, o facem dintr-o singura bucata
-             movl $0, (%ebx, %ecx, 4)
+             movl $0, (%ebx, %eax, 4)
              jmp incheieCelula
          ramane_vie:
-             movl $1, (%ebx, %ecx, 4)
+             movl $1, (%ebx, %eax, 4)
              jmp incheieCelula
        incheieCelula:     
        incl columnIndex 
        jmp C_linieCurenta
        finalGeneratie: 
-          movl $0, columnIndex
+          movl $1, columnIndex
           incl lineIndex
           jmp C_linii
     finalPas:
@@ -215,28 +239,28 @@ continuare:
        #AICI E SEGFAULTUL!!!
        movl $1, lineIndex
        copiereLinii:
-         movl lineIndex, %ebx
-         decl %ebx
+         movl columnIndex, %ebx
          cmp %ebx, m
-         je reiaPas
-         incl %ebx
+         jl reiaPas
          movl $1, columnIndex
          copiereCurenta:
            movl columnIndex, %ebx
-           decl %ebx
            cmp n, %ebx
-           je finalLinie
-           incl %ebx
+           jg finalLinie
            movl lineIndex, %eax
            xorl %edx, %edx
+           addl $2, n
            mull n
+           subl $2, n
            addl columnIndex, %eax
+           incl %eax
            lea newMatrix, %esi
            movl (%esi, %eax, 4), %ebx
            movl %ebx, (%edi, %eax, 4)
            incl columnIndex
+           jmp copiereCurenta
            finalLinie:
-              movl $0, columnIndex
+              movl $1, columnIndex
               incl lineIndex
               jmp copiereLinii              
     reiaPas:
@@ -244,7 +268,11 @@ continuare:
     jmp pasConway
 # o sa arate neuzual, deoarece matricea e indexata de la 1, ca sa se bordeze   
 scriere:
-    movl $1, lineIndex
+    movl deepM, %ebx
+    movl %ebx, m
+    movl deepN, %ebx
+    movl %ebx, n
+    movl $1, lineIndex #aici e Las Tres Marias pentru tema asta
     lea matrix, %edi
    linii:
      movl lineIndex, %ebx
@@ -258,10 +286,13 @@ scriere:
        jg finalScriere
        movl lineIndex, %eax
        xorl %edx, %edx
+       addl $2, n
        mull n
+       subl $2, n
        addl columnIndex, %eax
+       incl %eax
        #am pus in eax indicele unde scriem 
-       pushl (%edi, %eax, 4)/.
+       pushl (%edi, %eax, 4)
        pushl $formatWrite
        call printf
        popl %ebx
@@ -274,15 +305,20 @@ scriere:
        incl columnIndex
        jmp linieCurenta
      finalScriere:
-        pushl $newline
-        pushl $formatWrite
+        movl $newline, %ebx
+        pushl %ebx
+        pushl $formatString
         call printf
         popl %ebx
         popl %ebx
+        
+        pushl $0
+        call fflush
+        popl %ebx
+        
         movl $1, columnIndex
         incl lineIndex
         jmp linii
-        #scriem cu syscall un \n si ne intoarcem de unde am venit, pe linia urmatoare
 exit:
   movl $1, %eax
   movl $0, %ebx
